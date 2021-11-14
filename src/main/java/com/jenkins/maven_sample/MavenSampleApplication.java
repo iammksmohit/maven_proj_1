0544842d34cfc2b7e0928e5cac254e3e195f0d72
mohitsrivastava123 <mohit.srivastava@moveinsync.com>
2021-11-14 20:59:45 +0530

    First Commit for maven sample Jenkins Project

---
+++ src/main/java/com/jenkins/maven_sample/MavenSampleApplication.java
@@ -0,0 +1,13 @@
+package com.jenkins.maven_sample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MavenSampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MavenSampleApplication.class, args);
+    }
+
+}

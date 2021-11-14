001d5ef241832fd1fc5c2122e3637363f0390364
mohitsrivastava123 <mohit.srivastava@moveinsync.com>
2021-11-14 21:02:29 +0530

    Added Test Endpoint

--- src/main/java/com/jenkins/maven_sample/MavenSampleApplication.java
+++ src/main/java/com/jenkins/maven_sample/MavenSampleApplication.java
@@ -2,12 +2,22 @@ package com.jenkins.maven_sample;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
+@RequestMapping("/test")
 public class MavenSampleApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(MavenSampleApplication.class, args);
+    }
+
+    @GetMapping
+    public String jenkinsWorking(){
+        return "Sample Maven running on Jenkins";
     }
 
 }
